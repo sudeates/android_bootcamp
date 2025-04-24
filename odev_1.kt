a3cd2c93e104aae81b1806415c39fff185b6c63b
sudeates <115093746+sudeates@users.noreply.github.com>
2025-04-24 19:24:39 +0300

    Add files via upload

---
+++ odev_1.kt
@@ -0,0 +1,56 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+    var sehir = "ankara"
+    var ulke = "turkiye"
+    var postaKodu = 51000
+    var mail = "sdnrates2003@gmail.com"
+    val meslek = "bilgisayar mühendisi"
+    var stokMiktari = 30
+    var musteriAdi = "Sude ates"
+    var bakiye = 320
+    val dogumGunu = "07-05-2003"
+    var maas = 25000
+    var medeniDurum = "evli"
+    var urunYorum = "5 yıldız"
+    var odemeTarihi = "06-05-2025"
+    val odeme = 4000
+    var siparisAdeti = 3
+    var arabaModeli = "ford"
+    var kitapAdı = "calikusu"
+    val yayinlamaTarihi = "08-11-1998"
+    val indirimMiktari = "%50"
+    var odaSayisi = 30
+    var enlem = 100
+    var boylam = 20
+    var urunAdi = "ruj"
+    var yemekFiyati=100
+    var marka="zara"
+    val muzikAdi="someone like you"
+    var videoSuresi= 30
+    var urunPuani="4 yıldız"
+    var resimAdi="resim"
+    val dosyaFormati="pdf"
+    var renk="mor"
+    var renkKodu="erera"
+    val telefonModeli="iphone"
+    var ekranBoyutu="10 inch"
+    var agirlik=10
+    val ulusalGun="23 nisan"
+    var tatilGunu="1 mayıs"
+    val rezervasyonTarihi="04-04-2026"
+    var sokakAdi= "1433. sokak"
+    var otobusHattı=507
+    var kalanDakika=20
+    var takipKodu=11464
+    var kuponSuresi=20
+    var kuponKodu="INDIRIM20"
+    var faturaAdresi="sfsdfsdlfsd"
+    println("renk="+renk)
+
+}
+
+
+
+
+
